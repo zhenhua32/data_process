87f52736af04dac33de5b9a43eb00ae5d09306f6
zhenhua32 <zhenhua32@gmail.com>
2022-06-28 23:32:58 +0800

    save

---
+++ ali_nlp/cmd.py
@@ -0,0 +1,116 @@
+import time
+import json
+
+from client import Client, params_model
+
+
+class Throttle:
+    def __init__(self, rate):
+        self.rate = rate  # 限制速率
+        self.tokens = 0  # 当前剩余的令牌数
+        self.last = 0  # 上一次消费的时间
+
+    def consume(self, amount=1):
+        now = time.time()
+
+        if self.last == 0:
+            self.last = now
+
+        # 经过的时间
+        elapsed = now - self.last
+
+        # 当前剩余的令牌数, 不能一点点增加, 会超过任意间隔的 1 秒限制
+        if int(elapsed):
+            self.tokens += int(elapsed * self.rate)
+            self.last = now
+
+        # 当前的令牌数, 总之不能超过限制的速率
+        self.tokens = min(self.tokens, self.rate)
+
+        if self.tokens >= amount:
+            self.tokens -= amount
+        else:
+            amount = 0
+
+        return amount
+
+
+def test():
+    start = time.time()
+    client = Client(".env")
+    # 实际上用 20 还是会超过 QPS 限制
+    throttle = Throttle(rate=20)
+    with open("test.txt", "w", encoding="utf-8") as f:
+        for i in range(300):
+            # 没资源的时候, 返回 0, 所以结果是 True, 所以 while 会继续
+            while not throttle.consume():
+                time.sleep(1)
+            params = params_model.ReqGetWsChGeneral(Text="我是中国人")
+            resp = client.request(params)
+            f.write(json.dumps(resp, ensure_ascii=False) + "\n")
+    # 太坑了, 不知道怎么逼近极限, 现在 300 个需要 25 秒
+    print(time.time() - start)
+
+
+def test2():
+    start = time.time()
+    cur_time = time.time()
+    client = Client(".env")
+    rate = 20
+    cur_rate = 0
+    with open("test.txt", "w", encoding="utf-8") as f:
+        for i in range(300):
+            now = time.time()
+            print(i, now)
+            if now - cur_time > 1:
+                cur_rate = 1
+                cur_time = now
+            elif cur_rate < rate and cur_time + 1 > now:
+                cur_rate += 1
+            else:
+                # time.sleep(cur_time + 1 - now + 0.01)
+                time.sleep(1)
+                cur_time = time.time()
+                cur_rate = 1
+            params = params_model.ReqGetWsChGeneral(Text="我是中国人")
+            resp = client.request(params)
+            f.write(json.dumps(resp, ensure_ascii=False) + "\n")
+
+    # 还是个坑爹实现
+    print(time.time() - start)
+
+
+def test3():
+    start = time.time()
+    cur_time = time.time()
+    client = Client(".env")
+    rate = 20
+    cur_rate = 0
+    queue = [cur_time] * rate
+    with open("test.txt", "w", encoding="utf-8") as f:
+        for i in range(300):
+            now = time.time()
+            print(i, now)
+            if now - cur_time > 1 and queue[cur_rate] + 1 < now:
+                cur_rate = 1
+                cur_time = now
+            elif cur_rate < rate and cur_time + 1 > now and queue[cur_rate] + 1 < now:
+                cur_rate += 1
+            else:
+                time.sleep(cur_time + 1 - now)
+                # time.sleep(1)
+                cur_time = time.time()
+                cur_rate = 1
+            # 放入当前第 N 个位置的时间
+            queue[cur_rate - 1] = time.time()
+            params = params_model.ReqGetWsChGeneral(Text="我是中国人")
+            resp = client.request(params)
+            f.write(json.dumps(resp, ensure_ascii=False) + "\n")
+
+    print(time.time() - start)
+
+
+if __name__ == "__main__":
+    # test()
+    # test2()
+    test3()
